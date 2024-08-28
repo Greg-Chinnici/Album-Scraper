23afeadb64aea046971fad9ae6368fdce92729ae
Greg-Chinnici <chinnici@chapman.edu>
2024-08-28 12:31:22 -0700

    added new endpoint for successful search

--- website/app.py
+++ website/app.py
@@ -1,13 +1,16 @@
+from werkzeug.utils import redirect
 from AppleScraper import GetAlbum as GetAppleMusic
 from SpotifyScraper import GetAlbum as GetSpotify
 from SoundcloudScraper import GetAlbum as GetSoundcloud
-from flask import Flask, render_template, request, session
+
+from flask import Flask, render_template, request, session, url_for
 from markupsafe import escape
-from Album import Album
 
 
 
 app = Flask(__name__)
+app.secret_key = 'randomthingeventually'
+
 
 @app.route("/")
 def home_page():
@@ -17,13 +20,13 @@ def home_page():
 
 @app.route('/', methods=['GET', 'POST'])
 def search():
-    data = None
     if request.method == 'POST':
         alb = {}
         choice = request.form.get('ScrapeOption')
         searchTerm = str(escape(request.form["album_search"]))
         if not choice:
             return home_page()
+
         match choice:
             case "Spotify":
                 alb = GetSpotify(searchTerm)
@@ -31,14 +34,22 @@ def search():
                 alb = GetAppleMusic(searchTerm)
             case "Soundcloud":
                 alb = GetSoundcloud(searchTerm)
+        session["album"] = alb
+        session.modified = True
 
-        return render_template('poster.html',
-            name=alb['name'],
-            artist=alb['artist'],
-            year=alb['year'],
-            cover=alb['cover'],
-            songs=alb['songs'])
+        return redirect(url_for('found'))
+    else:
+        return home_page()
 
-    return home_page()
+@app.route('/found')
+def found():
+    alb = session.get('album')
+
+    return render_template('poster.html',
+        name=alb['name'],
+        artist=alb['artist'],
+        year=alb['year'],
+        cover=alb['cover'],
+        songs=alb['songs'])
 
 app.run(debug=True)
